e54a3c16f71b5d47e0e3b04badc33757b969a74b
kirito <250577914@qq.com>
2018-05-12 17:32:15 +0800

    init project structure

--- java-socket-consumer/src/main/java/moe/cnkirito/dubbojs/consumer/SocketConsumer.java
+++ java-socket-consumer/src/main/java/moe/cnkirito/dubbojs/consumer/SocketConsumer.java
@@ -11,9 +11,25 @@ import java.net.Socket;
  */
 public class SocketConsumer {
 
-    protected static final short MAGIC = (short) 0xdabb;
-    protected static final byte MAGIC_HIGH = short2bytes(MAGIC)[0];
-    protected static final byte MAGIC_LOW = short2bytes(MAGIC)[1];
+    public static final short MAGIC = (short) 0xdabb;
+    public static final byte MAGIC_HIGH = short2bytes(MAGIC)[0];
+    public static final byte MAGIC_LOW = short2bytes(MAGIC)[1];
+
+    //header length
+    public static final int DUBBO_HEADER_LENGTH = 16;
+
+    //com.alibaba.dubbo.common.serialize.support.hessian.Hessian2Serialization
+    public static final byte HESSIAN2_SERIALIZATION_CONTENT_ID = 2;
+
+    //dubbo最大的body序列化数据的大小
+    //com.alibaba.dubbo.common.Constants.DEAULT_PAY_LOAD
+    public static final int DEFAULT_PAYLOAD = 8 * 1024 * 1024;
+
+    protected static final byte FLAG_REQUEST = (byte) 0x80;
+    protected static final byte FLAG_TWOWAY = (byte) 0x40;
+    protected static final byte FLAG_EVENT = (byte) 0x20;
+    protected static final int SERIALIZATION_MASK = 0x1f;
+
 
     public static byte[] short2bytes(short v) {
         byte[] ret = {0, 0};
